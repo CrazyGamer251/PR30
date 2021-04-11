6dcddae0a9057973197cdc44ed251fbb0bb4fe82
bobjellybean <69946226+bobjellybean@users.noreply.github.com>
2021-04-11 11:12:40 +0530

    Update Box.js

--- Project 30/TowerSiege-1-main/Box.js
+++ Project 30/TowerSiege-1-main/Box.js
@@ -14,13 +14,18 @@ class Block {
     }
     display(){
       if(this.body.speed < 3){
-       
+         translate(this.body.position.x,this.body.position.y);
+         rotate(this.body.angle);
+         rectMode(CENTER);
+         rect(0,0, this.width, this.height);
        }
        else{
          World.remove(world, this.body);
          push();
          this.Visiblity = this.Visiblity - 5;
          tint(255,this.Visiblity);
+         rectMode(CENTER);
+         rect(this.body.position.x,this.body.position.y, this.width, this.height);
          pop();
        }
       }
